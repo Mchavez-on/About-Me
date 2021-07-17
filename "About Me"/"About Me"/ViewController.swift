1ec71591b0bf9625ab8ce588c5b5863efdccdd9e
Mariana Chavez <1069756@etusd.org>
2021-07-16 21:19:07 -0700

    First Commit

---
+++ "About Me"/"About Me"/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  "About Me"
+//
+//  Created by Mariana Chavez on 7/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func learnMoreTapped(_ sender: UIButton) {
+        // This is where I think I have reveal 3 fun facts about me
+        //Adding views:https://youtu.be/T_cJ8J2tdRA
+    }
+    
+
+}
+
